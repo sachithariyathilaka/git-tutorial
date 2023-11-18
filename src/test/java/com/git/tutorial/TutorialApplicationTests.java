442129e9a71640467a7e95c426ba40813ab566d6
sachith.vidu@yahoo.com <sachith.vidu@yahoo.com>
2023-11-18 13:53:21 +0530

    Project initial setup done

---
+++ src/test/java/com/git/tutorial/TutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
