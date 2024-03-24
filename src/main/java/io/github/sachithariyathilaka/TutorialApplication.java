9c88d111ed27b58f7792ab4f714fafa10db5a0d4
sachith.vidu@yahoo.com <sachith.vidu@yahoo.com>
2024-03-24 20:04:41 +0530

    Config update

--- src/main/java/io/github/sachithariyathilaka/TutorialApplication.java
+++ src/main/java/io/github/sachithariyathilaka/TutorialApplication.java
@@ -1,4 +1,4 @@
-package com.git.tutorial;
+package io.github.sachithariyathilaka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
